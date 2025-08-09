e7a4eb4b319859ac399b5253f1b084858e595e3a
Tom <toalcoja@gmail.com>
2025-08-09 16:04:27 +0100

    Update schemas

--- src/schemas/mapSchemas.ts
+++ src/schemas/mapSchemas.ts
@@ -1,13 +1,11 @@
 import { z } from "zod";
 
 export const MapContainerPropsSchema = z.object({
-  width: z.string().optional().default("100%"),
-  height: z.string().optional().default("500px"),
-  centre: z
-    .tuple([z.number(), z.number()])
-    .optional()
-    .default([-0.1278, 51.5074]),
-  zoom: z.number().min(1).max(20).optional().default(10),
+  width: z.string().default("100%"),
+  height: z.string().default("500px"),
+  centre: z.tuple([z.number(), z.number()]).default([-0.1278, 51.5074]),
+  zoom: z.number().min(1).max(20).default(10),
+  className: z.string().optional(),
 });
 
 export type MapContainerProps = z.infer<typeof MapContainerPropsSchema>;
