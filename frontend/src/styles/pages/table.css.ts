5c4685bb056b3c8a67ff05493a3d1d7e94c46cc8
Tom <toalcoja@gmail.com>
2025-08-11 20:35:12 +0100

    Fix mobile mode

--- frontend/src/styles/pages/table.css.ts
+++ frontend/src/styles/pages/table.css.ts
@@ -20,6 +20,11 @@ export const pageContainer = style({
     [shadowMd]:
       "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
   },
+  width: "100vw",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "stretch",
+  justifyContent: "center",
   maxWidth: "1400px",
   margin: "0 auto",
   padding: "2rem",
